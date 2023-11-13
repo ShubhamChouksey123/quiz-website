b617606b44a1c5fc857f46407b27737b25047417
ShubhamChouksey123 <shubhamchouksey1998@gmail.com>
2023-11-13 14:18:26 +0530

    added .env files and using the env variables in the code

--- start.sh
+++ start.sh
@@ -5,21 +5,17 @@
 #   Make sure to do it from the root directory,  
 
 #   Killing app service  
-# npx kill-port 8080
+npx kill-port 8080
 
 
 #   Building app - Maven
 echo "Building app with maven..."
-
 cd app
 ./mvnw spotless:apply
 ./mvnw clean install
-# ./app/mvnw clean install -DskipTests
-
-# mvn spotless:apply
-
-# mvn clean install 
-
+cd ..
 echo "Successful Build of app with maven"
 
-echo "Starting wallet link app ..."
+# Stating the quiz app - Maven
+echo "Starting quiz app ..."
+env $(cat app/.env | grep -v "^#" | xargs) java -jar app/target/app-0.0.1-SNAPSHOT.jar
