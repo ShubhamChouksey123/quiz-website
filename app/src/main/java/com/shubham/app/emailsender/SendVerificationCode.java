dd151269bd717782bb754d07a85a1b6390bfa914
ShubhamChouksey123 <shubhamchouksey1998@gmail.com>
2023-12-14 23:16:40 +0530

    added thankyou email

--- app/src/main/java/com/shubham/app/emailsender/SendVerificationCode.java
+++ app/src/main/java/com/shubham/app/emailsender/SendVerificationCode.java
@@ -20,14 +20,19 @@ public class SendVerificationCode {
 
     private static final String TEMPLATE_NAME = "recover-account-verify-phone";
     private static final String EMAIL_SUBJECT = "Wallet Financial - Recover Account OTP";
+
+    private static final String TEMPLATE_NAME_THANK_YOU_FOR_CONTACTING = "thank-you-contact";
+    private static final String EMAIL_SUBJECT_THANK_YOU_FOR_CONTACTING = "Thank you for contacting!";
+
     private static final String EMAIL_SUBJECT_CONTACT_QUERY = "Somebody wants to connect to you!";
 
     /** both uses same resources */
     public static Map<String, Resource> PARAMETER_RESOURCE_MAP_REGISTER_AC = Map.ofEntries(
             entry("logo", new ClassPathResource("templates/email-templates/register-account-verify-phone/Logo.png")));
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+    public static Map<String, Resource> PARAMETER_RESOURCE_MAP_EMAIL_AC = new HashMap<>();
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
     @Autowired
     private EmailSenderService emailSenderService;
 
@@ -51,10 +56,32 @@ public class SendVerificationCode {
         return emailSenderService.sendHtmlEmail(emailInformation);
     }
 
+    private boolean sendThankYouMail(String receiverPersonalName, String receiverEmail) {
+
+        if (receiverEmail == null) {
+            return false;
+        }
+
+        Map<String, Object> parameterMap = new HashMap<>();
+        if (receiverPersonalName != null) {
+            parameterMap.put("salutation", "Hi " + receiverPersonalName);
+        } else {
+            parameterMap.put("salutation", "Dear Customer");
+        }
+
+        EmailInformation emailInformation = new EmailInformation(receiverPersonalName, receiverEmail,
+                EMAIL_SUBJECT_THANK_YOU_FOR_CONTACTING, parameterMap, TEMPLATE_NAME_THANK_YOU_FOR_CONTACTING,
+                PARAMETER_RESOURCE_MAP_EMAIL_AC);
+
+        return emailSenderService.sendHtmlEmail(emailInformation);
+    }
+
     public void sendSMSAndEmail(String verificationCode, String countryCode, String phone, String receiverPersonalName,
             String email) {
 
         boolean smsSentStatus = true;
-        boolean emailSentStatus = sendEmail(verificationCode, receiverPersonalName, email);
+        // boolean emailSentStatus = sendEmail(verificationCode, receiverPersonalName,
+        // email);
+        sendThankYouMail(receiverPersonalName, email);
     }
 }
