2251a8eaa990cf2eaec71af31f7748d21eaaf0c9
ShubhamChouksey123 <shubhamchouksey1998@gmail.com>
2024-04-01 13:00:12 +0530

    added initial commit for cover letter generator

---
+++ app/src/main/java/com/shubham/app/render/RenderCoverTemplateImpl.java
@@ -0,0 +1,39 @@
+package com.shubham.app.render;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.shubham.app.emailsender.PrepareAndSendEmail;
+import com.shubham.app.hibernate.dao.ContactQueryDao;
+import com.shubham.app.service.questioncrud.QuestionCrud;
+import com.shubham.app.service.questioncrud.QuestionsUtils;
+
+@Service
+public class RenderCoverTemplateImpl implements RenderCoverTemplate {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Autowired
+    private QuestionCrud questionCrud;
+    @Autowired
+    private QuestionsUtils questionsUtils;
+    @Autowired
+    private ContactQueryDao contactQueryDao;
+    @Autowired
+    private PrepareAndSendEmail prepareAndSendEmail;
+
+    @Override
+    public void renderCoverForm(Model model) {
+    }
+
+    /**
+     * used to change the approval level of the question between : NEW, DISCARD,
+     * EDIT
+     */
+    @Override
+    public void renderCoverTemplate(String hiringManagerName, String companyName, String jobTitle) {
+    }
+}
