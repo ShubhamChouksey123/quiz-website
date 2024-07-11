39b63fbcbbdd0afda67f9247995b35d353ef04c7
ShubhamChouksey123 <shubhamchouksey1998@gmail.com>
2024-07-11 20:16:40 +0530

    Added Twitter LLD

---
+++ app/src/main/java/com/shubham/app/rizzle/dao/UserRepository.java
@@ -0,0 +1,72 @@
+package com.shubham.app.rizzle.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shubham.app.rizzle.entity.Post;
+import com.shubham.app.rizzle.entity.Post_;
+import com.shubham.app.rizzle.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+@Repository
+@Transactional
+public class UserRepository {
+
+    @Autowired
+    private EntityManager em;
+
+    public void save(User user) {
+        em.merge(user);
+    }
+
+    public User getUserById(Long userId) {
+        // try catch
+        return em.find(User.class, userId);
+    }
+
+    public void followerUser(Long userId, Long followerId) {
+
+        User user = getUserById(userId);
+        User follower = getUserById(followerId);
+        user.getFollowers().add(follower);
+
+        save(user);
+    }
+
+    public List<Post> fetchPostsOfFollowers(Long userId) {
+
+        User user = getUserById(userId);
+        List<User> followers = user.getFollowers();
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Post> query = cb.createQuery(Post.class);
+        Root<Post> root = query.from(Post.class);
+
+        query.select(root);
+
+        CriteriaBuilder.In<Long> inClause = cb.in(root.get(Post_.userId));
+        for (User follower : followers) {
+            inClause.value(follower.getUserId());
+        }
+        query.where(inClause);
+
+        List<Post> posts = new ArrayList<>();
+        try {
+            posts = em.createQuery(query).getResultList();
+        } catch (NoResultException e) {
+            System.out.println("No post to show");
+        } catch (Exception e) {
+            System.out.println("Couldn't find a questions with cause : " + e.getMessage());
+        }
+
+        return posts;
+    }
+}
---
+++ app/src/main/java/com/shubham/app/rizzle/service/TwitterService.java
@@ -0,0 +1,49 @@
+package com.shubham.app.rizzle.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shubham.app.rizzle.dao.PostRepository;
+import com.shubham.app.rizzle.dao.UserRepository;
+import com.shubham.app.rizzle.entity.Post;
+import com.shubham.app.rizzle.entity.User;
+
+import java.util.List;
+
+@Service
+public class TwitterService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public void createPost(Long userId, String text) {
+
+        User user = userRepository.getUserById(userId);
+
+        Post post = new Post(userId, text);
+        postRepository.save(post);
+    }
+
+    /** follow user */
+    public void followUser(Long userId, Long followerId) {
+
+        userRepository.followerUser(userId, followerId);
+    }
+
+    /**
+     * fetchFeed
+     *
+     * <p>
+     * fetch feeds from the latest follower to last follower
+     */
+    public List<Post> fetchFeed(Long userId) {
+
+        List<Post> posts = userRepository.fetchPostsOfFollowers(userId);
+        System.out.println("posts : " + posts);
+
+        return posts;
+    }
+}
